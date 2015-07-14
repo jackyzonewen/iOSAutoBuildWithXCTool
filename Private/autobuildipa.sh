edeaecd1bdc72480e9f55076e25ffad54cfa627d
jackyzonewen <jackyzonewen@gmail.com>
2015-07-14 18:13:48 +0800

    添加自动编译文件

---
+++ Private/autobuildipa.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+SHELL_DIR=$(cd `dirname $0`; pwd)
+
+#参数校验
+if [ $# -lt 2 ]; then
+	echo "调用自动打包的脚本的参数个数有误,例如:./autobuildipa.sh sourcepath [ipa_filepath] [buildsetting.plist's filepath] [build_cache_path]"
+	exit 1
+fi
+
+#1.读取工程路径
+XCODEPROJ_FOLDER_PATH="$1"
+echo "-> 工程路径:"$XCODEPROJ_FOLDER_PATH
+
+#2.ipa路径
+IPA_PATH=$SHELL_DIR/$TARGET.ipa
+#如果存在2个参数，那么第二个参数为ipa的路径
+if [ $# -gt 1 ]; then
+	IPA_PATH="$2"
+	echo "-> ipa输出路径:"$IPA_PATH
+fi
+
+#3.编译配置文件路径
+BUILDSETTINGFILE="$SHELL_DIR/buildsetting.plist"
+#如果有自定义配置文件路径
+if [ $# -gt 2 ]; then
+	BUILDSETTINGFILE="$3"
+fi
+echo "-> 编译配置文件路径:"$BUILDSETTINGFILE
+
+#4.编译临时文件路径
+XCODE_BUILD_PATH="../buildtmp" #"$(pwd)/build"
+if [ $# -gt 3 ]; then
+	XCODE_BUILD_PATH="$4"
+fi
+
+#读取Plist函数
+function readItem()
+{
+	if [ $# -lt 2 ]; then
+		echo "usage : readItem setting.plist itemName"
+	fi
+	
+	plistFilePath=$1
+	itemName=$2
+
+	/usr/libexec/PlistBuddy -c "print :$2" $1
+}
+
+#5.读取编译配置文件
+SDK=`readItem $BUILDSETTINGFILE SDK_VERSION`
+ARCH=`readItem $BUILDSETTINGFILE ARCHITECTURES`
+TARGET=`readItem $BUILDSETTINGFILE TARGET`
+CONFIGURATION=`readItem $BUILDSETTINGFILE CONFIGURATION`
+CODE_SIGN_IDENTITY=`readItem $BUILDSETTINGFILE CODE_SIGN_IDENTITY`
+PROVISIONING_PROFILE=`readItem $BUILDSETTINGFILE PROVISIONING_PROFILE`
+BUILD_TYPE=`readItem $BUILDSETTINGFILE PROJECT_TYPE`
+GCC_PREPROCESSOR_DEFINITIONS=`readItem $BUILDSETTINGFILE GCC_PREPROCESSOR_DEFINITIONS`
+
+pushd $(pwd)
+cd $XCODEPROJ_FOLDER_PATH
+
+errorcode=0
+
+#############安装brew 和xctool#############
+if which brew 2>/dev/null; then
+  echo "-> brew已经安装"
+else
+  echo "-> 检测到brew未安装,开始安装brew···"
+  curl -LsSf http://github.com/mxcl/homebrew/tarball/master | sudo tar xvz -C/usr/local --strip 1
+fi
+
+if which xctool>/dev/null; then
+  echo "-> xctool已经安装"
+else
+  echo "-> 检测到xctool未安装,开始安装xctool···"
+  sudo brew update
+  sudo brew install xctool 
+fi
+
+# build前清除缓存目录
+if [ $XCODE_BUILD_PATH  ] && [ -d $XCODE_BUILD_PATH ]; then
+	rm -rf $XCODE_BUILD_PATH
+fi
+
+#使用xctool自动打包
+schemeName=$TARGET
+
+#编译代码
+#/usr/bin/xcodebuild \
+#	-target $TARGET \
+#	-configuration $CONFIGURATION \
+#	-arch $ARCH \
+#	-sdk "$SDK" \
+#	CODE_SIGN_IDENTITY="$CODE_SIGN_IDENTITY" \
+#	PROVISIONING_PROFILE="$PROVISIONING_PROFILE" \
+#	CONFIGURATION_BUILD_DIR="$XCODE_BUILD_PATH" \
+#	GCC_PREPROCESSOR_DEFINITIONS="$GCC_PREPROCESSOR_DEFINITIONS" \
+#	clean \
+#	build
+
+if [ "$BUILD_TYPE" = "workspace" ];  then
+
+	echo "-> 开始编译工作空间"
+
+	project_path=$XCODEPROJ_FOLDER_PATH/$TARGET".xcworkspace"
+
+	xctool \
+		-workspace $project_path \
+		-scheme $schemeName \
+		-configuration $CONFIGURATION \
+		-sdk $SDK \
+		clean
+	xctool \
+		-workspace  $project_path \
+		-scheme $schemeName \
+		-configuration $CONFIGURATION \
+		-sdk $SDK \
+		CONFIGURATION_BUILD_DIR="$XCODE_BUILD_PATH" \
+		ONLY_ACTIVE_ARCH=NO \
+		build
+else
+	echo "-> 开始编译工程"
+
+	project_path=$XCODEPROJ_FOLDER_PATH/$TARGET".xcodeproj"
+
+	xctool \
+		-project $project_path \
+		-scheme $schemeName \
+		-configuration $CONFIGURATION \
+		-sdk $SDK \
+		clean
+	xctool \
+		-project  $project_path \
+		-scheme $schemeName \
+		-configuration $CONFIGURATION \
+		-sdk $SDK \
+		CONFIGURATION_BUILD_DIR="$XCODE_BUILD_PATH" \
+		ONLY_ACTIVE_ARCH=NO \
+		build
+fi
+
+errorcode=$?
+
+if [ ! $errorcode -eq 0 ]; then
+	echo "-> 编译失败"
+else
+	echo "-> 编译成功"
+
+	APP_BUILD_PATH=$(find $XCODE_BUILD_PATH -type d -name "*.app")
+	APP_dSYM_PATH=$(find $XCODE_BUILD_PATH -type d -name "*.app.dSYM")
+	IPA_dSYM_PATH=$IPA_PATH".dSYM"
+
+	echo "##############"
+
+	IPA_OUT_FOLDER="$(dirname "$IPA_PATH")"
+	if [ -d $IPA_OUT_FOLDER ]; then
+		rm -rf $IPA_OUT_FOLDER
+	fi
+	mkdir $IPA_OUT_FOLDER
+	
+	#打包程序 
+	#--sign $CODE_SIGN_IDENTITY --embed $PROVISIONING_PROFILE
+	echo "-> 开始打包···"
+	xcrun -sdk iphoneos PackageApplication -v $APP_BUILD_PATH -o $IPA_PATH 
+	errorcode=$?
+
+	if [ ! $errorcode -eq 0 ]; then
+		echo "-> 打包失败"
+	else
+		DSYM_OUT_FOLDER="$(dirname "$IPA_dSYM_PATH")"
+		if [ ! -d $DSYM_OUT_FOLDER ]; then
+			mkdir $DSYM_OUT_FOLDER
+		fi
+
+		cp -aR $APP_dSYM_PATH $IPA_dSYM_PATH
+		echo "-> 打包成功"
+
+		echo "-> 导出的ipa路径:"$IPA_PATH
+		echo "-> 编译成功的app路径:"$APP_BUILD_PATH
+		echo "-> 编译成功的dSYM路径:"$APP_dSYM_PATH
+	fi
+fi
+
+# 打包完成后清除缓存目录
+if [ $XCODE_BUILD_PATH  ] && [ -d $XCODE_BUILD_PATH ]; then
+	rm -rf $XCODE_BUILD_PATH
+fi
+
+exit $errorcode;
+popd
---
+++ WFT-autobuildall.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+SHELL_DIR=$(cd `dirname $0`; pwd)
+ 
+	# 1.修改配置文件（xxx-buildsetting.plist）配置内容，具体修改如下
+	# 	1）SDK_VERSION
+	# 		当前你可以编译sdk版本（使用xcodebuild -showsdks）
+	# 	2）ARCHITECTURES
+	# 		cpu架构（armv7, arm64）ps:暂不支持，直接根据XCode设置
+	# 	3）TARGET
+	# 		你工程的target是什么名字就填什么名字
+	# 	4）CONFIGURATION
+	# 		目前使用Release, 可以为Debug,AdHoc,Release,Distribution
+	# 	5）CODE_SIGN_IDENTITY*
+	# 		你的工程对应的签名id，（xcode点击工程文件，再点击target，选择build setting，单击Code Signing Identity的键值，选择Other，选择出现的字符串）
+	# 	6）PROVISIONING_PROFILE*
+	# 		你的工程授权文件的id值，（xcode点击工程文件，再点击target，选择build setting，单击Provisioning Profile的键值，选择Other，选择出现的字符串）
+	# 	7）GCC_PREPROCESSOR_DEFINITIONS
+	# 		如果有需要预处理的宏定义，需要在这里指定
+	# 	8）PROJECT_TYPE
+	# 		工程类型，如果用的是xcworkspace的目录，填写workspace，如果是xcodeproj，可不填
+	# 		
+
+	# 2.修改本文件的SOURCE_CODE_FOLDER,xcodepro或者xcworkspace
+	# XC所在目录
+
+	# 3.修改本文件的PROJECT_NAME，工程的名称
+
+	# 4. ipa默认生成在Output目录下面，会根据当前的编译时间生成文件夹
+	# 
+
+#代码路径（xcodeproj或者xcworkspace所在的目录）
+SOURCE_CODE_FOLDER="/Users/jackyzonewen/workspace/ios/WFT"
+PROJECT_NAME="WFT"
+
+#xcode编译临时文件的存储路径
+XCODE_BUILD_FOLDER="$SHELL_DIR/xcodebuildcache"
+
+#生成ipa的位置
+APP_BUILD_TIME=$(date +%Y%m%d%H%M) #编译时间
+
+#输出的根目录
+OUTPUT_ROOT_PATH="$SHELL_DIR/Output"
+if [ ! -d $OUTPUT_ROOT_PATH ]; then
+	mkdir $OUTPUT_ROOT_PATH
+fi
+
+STORE_IPA_PATH="$OUTPUT_ROOT_PATH/${PROJECT_NAME}_$APP_BUILD_TIME/${PROJECT_NAME}_adhoc.ipa"
+
+#自动编译并且打包签名
+COMPILE_IPA_PATH="$SHELL_DIR/${PROJECT_NAME}-buildsetting.plist" #编译配置文件
+
+echo "#### 开始自动打包服务···"
+
+sh $SHELL_DIR/Private/autobuildipa.sh $SOURCE_CODE_FOLDER $STORE_IPA_PATH $COMPILE_IPA_PATH $XCODE_BUILD_FOLDER
+
+#分目录
+IPA_OUT_FOLDER="$(dirname "$STORE_IPA_PATH")"
+
+DSYM_OUT_FOLDER="$(dirname "$STORE_IPA_PATH")_dSYM"
+
+if [ ! -d $DSYM_OUT_FOLDER ]; then
+	mkdir $DSYM_OUT_FOLDER
+fi
+
+mv $IPA_OUT_FOLDER/*.dSYM $DSYM_OUT_FOLDER
+
+echo "#### 自动打包服务执行结束!"
+
+
+
